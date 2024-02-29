91f46f95c2a9ce98bbfab7f82474e5ca6c875368
David Taylor <dktaylor1701@gmail.com>
2024-02-28 23:35:36 -0500

    Cleanup

--- Sources/Goose/ArrayExtensions/Array+Identifiable.swift
+++ Sources/Goose/ArrayExtensions/Array+Identifiable.swift
@@ -1,6 +1,6 @@
 //
 //  Array+Identifiable.swift
-//  
+//
 //
 //  Created by David Taylor on 12/28/22.
 //
@@ -10,32 +10,32 @@ import Foundation
 @available(macOS 10.15, *)
 @available(iOS 13.0, *)
 @available(watchOS 6.0, *)
-public extension Array where Element: Identifiable {
-    var ids: [Element.ID] {
-        self.map { $0.id }
-    }
-    
-    func index(of element: Element) -> Int? {
-        firstIndex(where: { $0.id == element.id })
-    }
-    
-    func item(with id: Element.ID) -> Element? {
-        first(where: { $0.id == id })
-    }
-    
-    func contains(id: Element.ID) -> Bool {
-        contains(where: {$0.id == id})
-    }
-    
-    mutating func delete(_ element: Element) {
-        guard let index = index(of: element) else { return }
-        
-        remove(at: index)
-    }
-    
-    mutating func update(_ element: Element) {
-        guard let index = index(of: element) else { return }
-        
-        self[index] = element
-    }
+extension Array where Element: Identifiable {
+  public var ids: [Element.ID] {
+    self.map { $0.id }
+  }
+
+  public func index(of element: Element) -> Int? {
+    firstIndex(where: { $0.id == element.id })
+  }
+
+  public func item(with id: Element.ID) -> Element? {
+    first(where: { $0.id == id })
+  }
+
+  public func contains(id: Element.ID) -> Bool {
+    contains(where: { $0.id == id })
+  }
+
+  public mutating func delete(_ element: Element) {
+    guard let index = index(of: element) else { return }
+
+    remove(at: index)
+  }
+
+  public mutating func update(_ element: Element) {
+    guard let index = index(of: element) else { return }
+
+    self[index] = element
+  }
 }
--- Sources/Goose/ArrayExtensions/Array+Sorted.swift
+++ Sources/Goose/ArrayExtensions/Array+Sorted.swift
@@ -1,6 +1,6 @@
 //
 //  Array+Sorted.swift
-//  
+//
 //
 //  Created by David Taylor on 12/28/22.
 //
@@ -8,41 +8,43 @@
 import Foundation
 
 public enum SortDirection {
-    case ascending
-    case descending
+  case ascending
+  case descending
 }
 
-public extension Array {
-    func sorted<U: Comparable>(by property: KeyPath<Element, U>, direction: SortDirection = .ascending) -> Array<Element> {
-        sorted {
-            switch direction {
-            case .ascending:
-                return $0[keyPath: property] < $1[keyPath: property]
-            case .descending:
-                return $0[keyPath: property] > $1[keyPath: property]
-            }
-        }
+extension Array {
+  public func sorted<U: Comparable>(
+    by property: KeyPath<Element, U>, direction: SortDirection = .ascending
+  ) -> [Element] {
+    sorted {
+      switch direction {
+      case .ascending:
+        return $0[keyPath: property] < $1[keyPath: property]
+      case .descending:
+        return $0[keyPath: property] > $1[keyPath: property]
+      }
     }
+  }
 }
 
 @propertyWrapper
 public struct Sorted<T, U> where U: Comparable {
-    private var value: [T]
-    private let keyPath: KeyPath<T, U>
-    private let direction: SortDirection
+  private var value: [T]
+  private let keyPath: KeyPath<T, U>
+  private let direction: SortDirection
 
-    public init(wrappedValue: [T], _ keyPath: KeyPath<T, U>, direction: SortDirection = .ascending) {
-        self.value = wrappedValue
-        self.keyPath = keyPath
-        self.direction = direction
-    }
+  public init(wrappedValue: [T], _ keyPath: KeyPath<T, U>, direction: SortDirection = .ascending) {
+    self.value = wrappedValue
+    self.keyPath = keyPath
+    self.direction = direction
+  }
 
-    public var wrappedValue: [T] {
-        get {
-            value.sorted(by: keyPath, direction: direction)
-        }
-        set {
-            value = newValue
-        }
+  public var wrappedValue: [T] {
+    get {
+      value.sorted(by: keyPath, direction: direction)
     }
+    set {
+      value = newValue
+    }
+  }
 }
